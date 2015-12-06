978cb19b6cb1d8fd79315123c19434e2b930ebab
Richard Gorman <richardgorman@outlook.com>
2015-12-07 00:14:44 +0100

    Refactored
    Rewrote functions and removed non-DOM related functions

--- DOM.js
+++ DOM.js
@@ -1,114 +1,55 @@
-/*
- *  DOM.js
- *
- *  Author: Richard Gorman
- *  Version: 1.1.0
- *
- *  @todo:
- *  - Add comments O_O
- *______________________*/
+//
+//  DOM.js
+//  Author: Richard Gorman
+//  Version: 1.2.0
+//
 
-
-
-
-(function( window ) {
+(function (window) {
 
     'use strict';
 
-    var DOM = function( selector, context ) {
-        return new DOM.init( selector, context );
-    };
-
-    var _isElement = function( obj ) {
-        return obj.nodeType === 1;
-    }
-
-    var _isDocument = function( obj ) {
-        return obj.nodeType === 9;
-    }
-
-    var _query = function( selector, context, p ) {
-        var match, matches;
-
-        context = context || document;
-
-        if ( typeof selector !== 'string' ) {
-            console.error('DOM - ' + selector + ' is not a valid selector.');
-            return p = false;
-        }
-
-        if ( ! _isElement( context ) && ! _isDocument( context ) ) {
-            console.error('DOM - ' + context + ' is not a valid scontext.');
-            return p = false;
-        }
-
-        matches = /^(?:#([\w\-]+)|(\w+)|\.([\w\-]+))$/.exec( selector );
-
-        if ( matches !== null ) {
-
-            // #id
-            if ( matches[1] !== undefined ) {
-
-                if ( _isDocument( context ) ) {
-                    match = context.getElementById( matches[1] );
-
-                    if ( match !== null ) {
-                        p.push( context.getElementById( matches[1] ) );
-                    }
-                }
-            }
-
-            // <tag>
-            else if (matches[2] !== undefined) {
-                [].push.apply( p, context.getElementsByTagName( matches[2] ) );
-            }
-
-            // .class
-            else if (matches[3] !== undefined) {
-                [].push.apply( p, context.getElementsByClassName( matches[3] ) );
-            }
-        }
-        else {
-            [].push.apply( p, context.querySelectorAll( selector ) );
-        }
-
-        return true;
+    var DOM = function (selector, context) {
+        return new DOM._query(selector, context);
     };
 
     DOM.fn = DOM.prototype = {
-       version: '1.1.0',
-       constructor: DOM
-   };
+        version: '1.1.0',
+        constructor: DOM,
+        
+        // Detect if browser supports {Node}.classList
+        classList: (function () {
+            return !!document.body.classList;
+        })()
+    };
 
-   /** Basic extend function */
-   DOM.fn.extend = DOM.extend = function() {
-       var target = this,
-           i = 0,
-           key;
+    DOM.fn.extend = DOM.extend = function () {
+        var target = this,
+            key;
 
-       if (arguments.length > 1) {
-           target = arguments[0];
-           i = 1;
-       }
+        if (arguments.length > 1) {
+            target = arguments[0];
+            i = 1;
+        }
 
-       for (; i < arguments.length; i++) {
-           for (key in arguments[i]) {
-               if (arguments[i].hasOwnProperty(key)) {
-                   target[key] = arguments[i][key];
-               }
-           }
-       }
+        for (var i = 0; i < arguments.length; i++) {
+            for (key in arguments[i]) {
+                if (arguments[i].hasOwnProperty(key)) {
+                    target[key] = arguments[i][key];
+                }
+            }
+        }
 
-       return target;
-   };
+        return target;
+    };
 
     DOM.extend({
 
-        each: function(object, callback) {
-            if (!object) {
+        _each: function (object, callback) {
+            // Stop if no object or callback given.
+            if (!object || !callback) {
                 return false;
             }
-
+            
             for (var i = 0; i < object.length; i++) {
                 callback.call(object[i], i);
             }
@@ -116,165 +57,139 @@
             return this;
         },
 
-        init: function( selector, context ) {
+        _query: function (selector, context) {
+            context = context || document;
 
-            // Stop here if no selector found.
-            if ( ! selector ) {
+            // Stop if no selector or blank selector found.
+            if (!selector || selector == "") {
                 return false;
             }
 
-            // Just return here if selector is already a DOM object.
-            if ( selector instanceof DOM ) {
+            // Return selector if is already a DOM instance.
+            if (selector instanceof DOM) {
                 return selector;
             }
-
-            this.length = 0;
-
-            // We need to borrow some array functions...
-            this.pop = [].pop;
-            this.push = [].push;
-            this.sort = [].sort;
-            this.splice = [].splice;
             
             // If the selector is a single node then return it.
-            if ( _isElement( selector ) || _isDocument( selector ) ) {
+            if (selector.nodeType === 1 || selector.nodeType === 9) {
                 this[0] = selector;
                 this.length = 1;
-
                 return this;
             }
 
-            // Okay so we actually have to go and find something...
-
-            // If selector is an Array then iterate, check for nodes, setup and return them.
-            if (Array.isArray(selector)) {
-                for (var i = selector.length; i--;) {
-                    if ( _isElement( selector[i] ) || _isDocument( selector[i] ) ) {
-                        this[i] = selector[i];
-                        this.length++;
-                    }
-                }
-
-                return this;
-            }
-
-            // Ok, the selector should be a string. Let's query the DOM.
-            _query( selector, context, this );
+            [].push.apply(this, context.querySelectorAll(selector));
 
             return this;
-        },
-
-        onanimationend: (function() {
-            var t, el = document.createElement('fakeelement'),
-
-            animations = {
-                'animation':'animationend',
-                'OAnimation':'oAnimationEnd',
-                'MozAnimation':'animationend',
-                'WebkitAnimation':'webkitAnimationEnd'
-            }
-
-            for (t in animations) {
-                if(el.style[t] !== undefined) {
-                    return animations[t];
-                }
-            }
-        })(),
-
-        ontransitionend: (function() {
-            var t, el = document.createElement('fakeelement'),
-
-            transitions = {
-                'transition':'transitionend',
-                'OTransition':'oTransitionEnd',
-                'MozTransition':'transitionend',
-                'WebkitTransition':'webkitTransitionEnd'
-            }
-
-            for (t in transitions) {
-                if(el.style[t] !== undefined) {
-                    return transitions[t];
-                }
-            }
-        })(),
-
-        touchsupport: (function() {
-            return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
-        })()
-
+        }
+        
     });
 
     DOM.prototype.extend({
 
-        each: function(callback) {
-            DOM.each(this, callback);
-
+        each: function (callback) {
+            DOM._each(this, callback);
             return this;
         },
 
-        find: function(selector) {
-            if ( !selector ) {
-                return false;
+        find: function (selector) {
+            return DOM(selector, this[0]);
+        },
+
+        addClass: (function () {
+            if (DOM.fn.classList) {
+                return function (name) {
+                    this.each(function () {
+                        this.classList.add(name);
+                    });
+                }
             }
-
-            console.log(this);
-
-            this.context = Array.prototype.slice.call(this, [0]);
-
-            while ( this.length > 0 ) {
-                this.pop();
+            else {
+                return function (name) {
+                    this.each(function () {
+                        this.className += ' ' + name;
+                    });
+                }
             }
+        })(),
 
-            for ( var i = this.context.length; i--; ) {
-                _query( selector, this.context[i], this );
+        removeClass: (function () {
+            if (DOM.fn.classList) {
+                return function (name) {
+                    this.each(function () {
+                        this.classList.remove(name);
+                    });
+                }
             }
+            else {
+                return function (name) {
+                    this.each(function () {
+                        this.className = this.className.replace(new RegExp('(^|\\b)' + name.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+                    });
+                }
+            }
+        })(),
 
-            return this;
-        },
+        hasClass: (function () {
+            if (DOM.fn.classList) {
+                return function (name) {
+                    return this[0].classList.contains(name);
+                }
+            }
+            else {
+                return function (name) {
+                    return new RegExp('(^| )' + name + '( |$)', 'gi').test(this.className);
+                }
+            }
+        })(),
 
-        addClass: function(name) {
-            return this.each(function() {
-                this.classList.add(name);
-            });
-        },
+        toggleClass: (function () {
+            if (DOM.fn.classList) {
+                return function (name) {
+                    this.each(function () {
+                        this.classList.toggle(name);
+                    });
+                }
+            }
+            else {
+                return function (name) {
+                    this.each(function () {
+                        var classes = this.className.split(' '),
+                            existingIndex = classes.indexOf(name);
 
-        removeClass: function(name) {
-            return this.each(function() {
-                this.classList.remove(name);
-            });
-        },
+                        if (existingIndex >= 0) {
+                            classes.splice(existingIndex, 1);
+                        }
+                        else {
+                            classes.push(name);
+                        }
 
-        hasClass: function(name) {
-            return this[0].classList.contains(name);
-        },
+                        this.className = classes.join(' ');
+                    });
+                }
+            }
+        })(),
 
-        toggleClass: function(name) {
-            return this.each(function() {
-                this.classList.toggle(name);
-            });
-        },
-
-        addEvent: function(event, callback) {
-            return this.each(function() {
+        addEvent: function (event, callback) {
+            return this.each(function () {
                 this.addEventListener(event, callback, false);
             });
         },
 
-        removeEvent: function(event, callback) {
-            return this.each(function() {
+        removeEvent: function (event, callback) {
+            return this.each(function () {
                 this.removeEventListener(event, callback, false);
             });
         },
 
-        getIndex: function(el) {
-            var list = Array.prototype.slice.call( this );
-            return list.indexOf( el );
+        getIndex: function (el) {
+            var list = Array.prototype.slice.call(this);
+            return list.indexOf(el);
         }
 
     });
 
-    DOM.init.prototype = DOM.prototype;
+    DOM._query.prototype = DOM.prototype;
 
     return window.DOM = DOM;
 
-})( window );
+})(window);
